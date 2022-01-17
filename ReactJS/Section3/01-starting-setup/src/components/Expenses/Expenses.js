4a70d57940d384e648d1ced1bdf33a092c7f91f5
TranDinhKhoi2002 <87485108+TranDinhKhoi2002@users.noreply.github.com>
2022-01-17 11:44:45 +0700

    ReactJS

--- ReactJS/Section3/01-starting-setup/src/components/Expenses/Expenses.js
+++ ReactJS/Section3/01-starting-setup/src/components/Expenses/Expenses.js
@@ -1,16 +1,19 @@
-import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import Card from "../UI/Card";
 import ExpensesFilter from "../ExpensesFilter/ExpensesFilter";
 import { useState } from "react/cjs/react.development";
+import ExpensesList from "./ExpensesList";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2022");
 
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
-    console.log("Expenses.js", selectedYear);
   };
+
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
 
   return (
     <div>
@@ -19,16 +22,7 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeYearHandler={filterChangeHandler}
         />
-        {props.items.map((item) => {
-          return (
-            <ExpenseItem
-              key={item.id}
-              title={item.title}
-              amount={item.amount}
-              date={item.date}
-            />
-          );
-        })}
+        <ExpensesList items={filteredExpenses}/>
       </Card>
     </div>
   );
---
+++ ReactJS/Section3/01-starting-setup/src/components/Expenses/ExpensesList.js
@@ -0,0 +1,25 @@
+import ExpenseItem from "./ExpenseItem";
+import "./ExpensesList.css";
+
+const ExpensesList = (props) => {
+  if (props.items.length === 0) {
+    return <p className="expenses-list__fallback">No expenses found!!!</p>;
+  }
+
+  return (
+    <ul className="expenses-list">
+      {props.items.map((item) => {
+        return (
+          <ExpenseItem
+            key={item.id}
+            title={item.title}
+            amount={item.amount}
+            date={item.date}
+          />
+        );
+      })}
+    </ul>
+  );
+};
+
+export default ExpensesList;
